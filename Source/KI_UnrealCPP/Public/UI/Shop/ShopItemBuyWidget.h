9a520db48d879f5b100d66c67289928f8bfacb06
go2665 <go2665@outlook.com>
2025-12-04 16:31:29 +0900

    UEditableTextBox 사용하기

--- Source/KI_UnrealCPP/Public/UI/Shop/ShopItemBuyWidget.h
+++ Source/KI_UnrealCPP/Public/UI/Shop/ShopItemBuyWidget.h
@@ -6,6 +6,11 @@
 #include "Blueprint/UserWidget.h"
 #include "ShopItemBuyWidget.generated.h"
 
+class UImage;
+class UTextBlock;
+class UEditableTextBox;
+class UButton;
+class UOverlay;
 /**
  * 
  */
@@ -13,5 +18,43 @@ UCLASS()
 class KI_UNREALCPP_API UShopItemBuyWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UFUNCTION()
+	void OnItemCountTextChanged(const FText& Text);
+
+	UFUNCTION()
+	void OnItemCountTextCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UImage> ItemIcon = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemName = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemPrice = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemStockCount = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemDescription = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> ItemCount = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UButton> ItemBuy = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemSell", meta = (BindWidget))
+	TObjectPtr<UOverlay> SoldOut = nullptr;
+
+private:
+	static const int32 MinimumItemCount = 1;
+
 };
